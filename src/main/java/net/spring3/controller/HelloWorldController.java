5e932eeba93491d7a3657a0c332843709dfa8c20
Manish <itsmanishagarwal@gmail.com>
2012-07-11 23:04:17 +0530

    resolved resources not copying to classes

---
+++ src/main/java/net/spring3/controller/HelloWorldController.java
@@ -0,0 +1,20 @@
+package net.spring3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@SessionAttributes
+public class HelloWorldController {
+
+	@RequestMapping(value = "/hello")
+	public ModelAndView helloWorld() {
+
+		String message = "Hello World, Spring 3.0!";
+		System.out.println(message);
+		return new ModelAndView("hello", "message", message);
+	}
+
+}
